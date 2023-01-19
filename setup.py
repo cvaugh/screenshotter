ce1ab85b1188b9b5d330a5e3bf4d0896de9cfa7b
Chase Vaughan <git@cvaugh.dev>
2023-01-19 01:47:45 -0500

    Minor fixes

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 from setuptools import find_packages, setup
+from pathlib import Path
 
 setup(
     name="screenshotter",
     version="0.1.0",
     license="MIT",
     description="Automatically take full-screen screenshots at a fixed rate",
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type="text/markdown",
     author="Chase Vaughan",
     author_email="contact@cvaugh.dev",
     packages=find_packages(),
